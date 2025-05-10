2035a350a7443dcb87c39981eeafa481006c5822
Chris Burgess <chris@giantrobot.co.nz>
2025-05-10 17:22:51 +1200

    Gitlab CI to Github Actions

--- config/renovate/config.js
+++ config/renovate/config.js
@@ -1,8 +1,33 @@
 // Configuration from environment, prefer renovate.json where possible.
 module.exports = {
-  endpoint: process.env.CI_API_V4_URL,
-  platform: 'gitlab',
-  repositories: [
-    process.env.CI_PROJECT_PATH
-  ],
+  ...getPlatformConfig(),
 };
+
+function getPlatformConfig() {
+  // Gitlab CI
+  if (process.env.CI_API_V4_URL && process.env.CI_SERVER_HOST) {
+    console.log('GitLab CI configuration');
+    return {
+      endpoint: process.env.CI_API_V4_URL,
+      platform: 'gitlab',
+      repositories: [process.env.CI_PROJECT_PATH],
+    };
+  }
+
+  // Github Actions
+  if (process.env.RENOVATE_CONFIG_FILE.match(/github/)) {
+    console.log('GitHub Actions configuration');
+    return {
+      platform: 'github',
+      repositories: ['xurizaemon/tmp-6da69'],
+    };
+  }
+
+  // Fallback
+  console.log('Fallback configuration');
+  return {
+    platform: process.env.RENOVATE_PLATFORM || 'github',
+    repositories: [process.env.RENOVATE_REPOSITORY || 'default-repository'],
+    token: process.env.RENOVATE_TOKEN,
+  };
+}
